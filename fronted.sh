c3bcdbd8c3660fd39a2a613bfec49df4cbbe5660
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-11-04 16:44:20 +0530

    commit push

--- fronted.sh
+++ fronted.sh
@@ -1,7 +1,7 @@
-echo -e "\e[32m installing nginx \e[om"
+echo -e "\e[32m installing nginx \e[0m"
 sudo dnf install nginx
 
-echo -e "\e[32m copying the repo file into expense.conf"
+echo -e "\e[32m copying the repo file into expense.conf \e[0m"
 cp expens.conf /etc/nginx/default.d/expense.conf
 
 echo -e "\e[32m removing default content of nginx \e[0m"
@@ -10,7 +10,7 @@ rm -rf /usr/share/nginx/html/*
 echo -e "\e[32m downloading the frontend content \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[32 unzip the download content by changing the location \e[0m"
+echo -e "\e[32m unzip the download content by changing the location \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
