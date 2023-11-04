c1da866f842b9265a925cea4846900415f44a4e5
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-11-04 22:18:23 +0530

    commit push

--- Backend.sh
+++ Backend.sh
@@ -1,5 +1,14 @@
 source common.sh
 
+MY_SQL_PASSWORD=$1
+
+if [ -z "$1" ]; then
+
+  echo -e "\e[31 input is missing \e[0m"
+  exit
+
+fi
+
 echo -e "\e[32m disable the default nodejs version \e[0m"
 dnf module disable nodejs -y &>>log_file
 check_status
@@ -16,21 +25,18 @@ echo -e "\e[32m copying the service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 check_status
 
-echo -e "\e[32m deleting the existing the user \e[0m"
-userdel -r expense &>>log_file
-check_status
+id expense &>>log_file
+if [ $? -nq 0 ]; then
+  echo -e "\e[32m deleting the existing the user \e[0m"
+  useradd expense &>>log_file
+  check_status
+fi
 
-echo -e "\e[32m adding the user \e[0m"
-useradd expense &>>log_file
-check_status
-
-echo -e "\e[32m removing the directory \e[0m"
-rm -rf /app &>>log_file
-check_status
-
-echo -e "\e[32m creating the app directory \e[0m"
-mkdir /app &>>log_file
-check_status
+if [ ! -d /app ]; then
+  echo -e "\e[32m creating the app directory \e[0m"
+  mkdir /app &>>log_file
+  check_status
+fi
 
 echo -e "\e[32m Download the application code \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
@@ -53,7 +59,7 @@ dnf install mysql -y &>>log_file
 check_status
 
 echo -e "\e[32m load the scheme \e[0m"
-mysql -h 172.31.43.125 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>log_file
+mysql -h 172.31.43.125 -uroot -p${MY_SQL_PASSWORD} </app/schema/backend.sql &>>log_file
 check_status
 
 echo -e "\e[32m starting the service \e[0m"
