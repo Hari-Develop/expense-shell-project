b78d1d68962e5a352cad1b9d1695cd9b7af91da7
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-11-04 16:54:56 +0530

    commit push

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 check_status() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[31m failed the script \e[0m"
-  else
     echo -e "\e[32m success the script \e[0m"
+  else
+    echo -e "\e[31m failed the script \e[0m"
   fi
 }
