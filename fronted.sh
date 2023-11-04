4933e936c952c92a9e9ab778b1595fa05ffc341b
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-11-04 23:09:08 +0530

    commit push

--- fronted.sh
+++ fronted.sh
@@ -1,23 +1,29 @@
 source common.sh
 
 echo -e "\e[32m installing nginx \e[0m"
-sudo dnf install nginx
+sudo dnf install nginx &>>log_file
+check_status
 
 echo -e "\e[32m copying the repo file into expense.conf \e[0m"
-cp expens.conf /etc/nginx/default.d/expense.conf
+cp expens.conf /etc/nginx/default.d/expense.conf &>>log_file
+check_status
 
 echo -e "\e[32m removing default content of nginx \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>log_file
+check_status
 
 echo -e "\e[32m downloading the frontend content \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+check_status
 
 echo -e "\e[32m unzip the download content by changing the location \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>>log_file
+unzip /tmp/frontend.zip &>>log_file
+check_status
 
 echo -e "\e[32m starting the nginx service \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
+check_status
 
 
--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,22 @@
 source common.sh
 
 echo -e "\e[32m disabling the default mysql \e[0m"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>log_file
+status_check
 
 echo -e "\e[32m copying the repo file  \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
+status_check
 
 echo -e "\e[32m installing the mysql \e[0m"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>log_file
+status_check
 
 echo -e "\e[32m setting up the root password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
+status_check
 
 echo -e "\e[32m starting the mysql service \e[32m"
-systemctl enable mysqld
-systemctl restart mysqld
+systemctl enable mysqld &>>log_file
+systemctl restart mysqld &>>log_file
+status_check
